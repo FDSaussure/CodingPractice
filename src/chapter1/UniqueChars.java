3f91a0a8e2591a163a9b085ee6ac4f13037ab8d0
FDSaussure <lswadesh@gmail.com>
2014-02-21 14:14:35 +0000

    Update

--- src/chapter1/UniqueChars.java
+++ src/chapter1/UniqueChars.java
@@ -8,44 +8,57 @@ public class UniqueChars {
 	public static boolean CheckUniqueBA(String input) {
 		if (input == null) { throw new IllegalArgumentException(); }
 		
-		boolean unique = true;
 		boolean[] seen = new boolean[65536];
 		
-		for (int i = 0; (i < input.length()) & unique; i++) {
+		for (int i = 0; (i < input.length()); i++) {
 			char curChar = input.charAt(i);
 			int index = Character.getNumericValue(curChar);
 			
 			if (seen[index]) {
-				unique = false;
+				return false;
 			}
 			else {
 				seen[index] = true;
 			}
 		}
 				
-		return unique;
+		return true;
 	}
 	
 	// Implementation using a HashMap.
 	public static boolean CheckUniqueHM(String input) {
 		if (input == null) { throw new IllegalArgumentException(); }
 		
-		boolean unique = true;
 		HashMap<Integer, Boolean> seen = new HashMap<Integer, Boolean>();
 		
-		for (int i = 0; (i < input.length()) & unique; i++) {
+		for (int i = 0; (i < input.length()); i++) {
 			char curChar = input.charAt(i);
 			int index = Character.getNumericValue(curChar);
 			
 			if (seen.containsKey(index)) {
-				unique = false;
+				return false;
 			}
 			else {
 				seen.put(index, true);
 			}
 		}
 				
-		return unique;
+		return true;
 
+	}
+	
+	// Implementation using no additional data structures.
+	public static boolean CheckUniqueNoStruct(String input) {
+		if (input == null) { throw new IllegalArgumentException(); }
+		
+		for (int i = 0; i < (input.length() - 1); i++) {
+			for (int j = i+1; j < input.length(); j++) {
+				if (input.charAt(i) == input.charAt(j)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
